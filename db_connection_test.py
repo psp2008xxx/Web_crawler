f2bf2fb48657cdbd915ecc6c6fda74d86b367a5a
kuni <kun.ni@nokia.com>
2016-08-15 14:14:20 +0800

    utf-8 issue fix

--- db_connection_test.py
+++ db_connection_test.py
@@ -1,39 +1,43 @@
 #!/usr/bin/python
-#coding=utf-8
-
+# -*- coding: utf-8 -*-
 
 import pymysql
 import urllib2
 import re
 from bs4 import BeautifulSoup
-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 
 base_url = "http://www.netbian.com"
-conn = pymysql.connect(host="127.0.0.1", user="root", passwd="password", db="scraping")
+conn = pymysql.connect(host="127.0.0.1", user="root", passwd="password", db="scraping", charset='utf8')
 cur = conn.cursor()
 cur.execute("USE scraping")
-cur.execute('SET NAMES utf8;')
-cur.execute('SET CHARACTER SET utf8;')
-cur.execute('SET character_set_connection=utf8;')
 
 
 def store_data(title, content):
-    cur.execute("INSERT TABLE pages(title, content) VALUES(\"%s\", \"%s\")", (title, content))
+    sql_command = "INSERT INTO pages (title, content) VALUES(\"%s\", \"%s\")" % (title.encode("utf-8"), content)
+    cur.execute(sql_command)
     cur.connection.commit()
+
 
 def get_links(url):
     sub_link_contents = {}
     html_content = urllib2.urlopen(url)
     bs4Obj = BeautifulSoup(html_content, "html.parser")
-    sub_urls = bs4Obj.findAll("a",{"href":re.compile(".*"), "title":re.compile(".*")})
+    sub_urls = bs4Obj.findAll("a", {"href": re.compile(".*"), "title": re.compile(".*")})
 
     for sub_url in sub_urls:
+        # sub_url.attrs["title"] = sub_url.attrs["title"].encode('utf-8')
+        # print sub_url.attrs["title"].__class__
         sub_link_contents[sub_url.attrs["title"]] = base_url + sub_url.attrs["href"]
     return sub_link_contents
 
+
 link_contents = get_links(base_url)
-for title,link_content in link_contents.items():
-    print title, link_content
+for title, link_content in link_contents.items():
     store_data(title, link_content)
+cur.close()
+conn.close()
 
